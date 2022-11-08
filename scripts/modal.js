5b10b5273fd86eaaeb0ec25e66d49f9282c75788
vherever <vherever@gmail.com>
2022-11-08 22:38:48 +0200

    add svg close icon to easy customize with css

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,3 +1,5 @@
+const iconCloseSVG = '<?xml version="1.0" encoding="UTF-8"?><svg class="" enable-background="new 0 0 512 512" version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><ellipse class="" cx="256" cy="256" rx="256" ry="255.83" fill="#e04f5f" style="" data-original="#e04f5f"/><g transform="matrix(-.7071 .7071 -.7071 -.7071 77.26 32)" fill="#fff"><rect class="" x="3.98" y="-427.62" width="55.992" height="285.67" style="" data-original="#ffffff"/><rect class="" x="-110.83" y="-312.82" width="285.67" height="55.992" style="" data-original="#ffffff"/></g></svg>\n';
+
 ;(function () {
   this.Modal = function () {
 
@@ -78,7 +80,18 @@
       this.closeButton = document.createElement('a');
       this.closeButton.href = 'javascript:;';
       this.closeButton.innerText = '';
-      this.closeButton.className = 'modal-close close-button';
+
+      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      const path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+      svg.setAttribute('viewBox', '0 0 50 50');
+      svg.setAttribute('class', 'modal-close close-button');
+      path.setAttribute("d","M28.228,23.986L47.092,5.122c1.172-1.171,1.172-3.071,0-4.242c-1.172-1.172-3.07-1.172-4.242,0L23.986,19.744L5.121,0.88   c-1.172-1.172-3.07-1.172-4.242,0c-1.172,1.171-1.172,3.071,0,4.242l18.865,18.864L0.879,42.85c-1.172,1.171-1.172,3.071,0,4.242   C1.465,47.677,2.233,47.97,3,47.97s1.535-0.293,2.121-0.879l18.865-18.864L42.85,47.091c0.586,0.586,1.354,0.879,2.121,0.879   s1.535-0.293,2.121-0.879c1.172-1.171,1.172-3.071,0-4.242L28.228,23.986z");
+      path.setAttribute('fill', '#e74c3c');
+      svg.appendChild(path);
+      this.closeButton.appendChild(svg);
+      // document.body.appendChild(svg);
+      
+      // this.closeButton.className = 'modal-close close-button';
       this.modal.appendChild(this.closeButton);
     }
 
